c916a7700cb077bab03d122ea638bfca62c4ebd4
DeanC <dean.clancy91@gmail.com>
2020-11-16 19:03:47 -0800

    Add getCurrentMonthFunction

--- src/api.js
+++ src/api.js
@@ -1,3 +1,14 @@
 // BASE URL
 const baseURL = process.env.REACT_APP_API_BASE_URL;
 
+// Get date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if(month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+}
+
+getCurrentMonth();
