39a69f0df02878dade1d3adef29256e7849aeb6f
DeanC <dean.clancy91@gmail.com>
2020-11-20 15:05:14 -0800

    Fix image in Game Card

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -16,8 +16,12 @@ function GameCard({ name, released, image }) {
 const StyledGameCard = styled(motion.div)`
   min-height: 30vh;
   box-shadow: 5px 5px 10px rgba(0, 0, 0, .2);
+  text-align: center;
+  border-radius: 5px;
   img {
     width: 100%;
+    height: 35vh;
+    object-fit: cover;
   }
 `;
 export default GameCard;
