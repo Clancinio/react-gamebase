d3cdb8702d4f304bc357a4df0653572a914a5261
DeanC <dean.clancy91@gmail.com>
2020-11-20 08:56:37 -0800

    Create GameCard component

---
+++ src/components/GameCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// Styling and Animation
+
+
+function GameCard() {
+  return (
+    <div>
+      <h3>Game Title</h3>
+      <p>Description</p>
+    </div>
+  )
+}
+
+export default GameCard
