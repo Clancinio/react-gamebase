6e0cfc3d3364a61a8523aca96091f285d8042953
DeanC <dean.clancy91@gmail.com>
2020-11-22 17:41:11 -0800

    Add initial styling for the game details

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -10,8 +10,8 @@ function GameDetail() {
   const { game, screen } = useSelector((state) => state.detail);
 
   return (
-    <div className="card-shadow">
-      <div className="details">
+    <CardShadow>
+      <Details>
         <div className="stats">
           <div className="rating">
             <h3>{game.name}</h3>
@@ -20,23 +20,54 @@ function GameDetail() {
           <div className="info">
             <h3>Platforms</h3>
             <div className="platforms">
-              {game.platforms.map((data) => (
-                <h3 key={data.platform.id}>{data.platform.name}</h3>
-              ))}
+              {game.platforms &&
+                game.platforms.map((data) => (
+                  <h3 key={data.platform.id}>{data.platform.name}</h3>
+                ))}
             </div>
           </div>
         </div>
         <div className="media">
           <img src={game.background_image} alt={game.name} />
         </div>
+        <div className="description">{game.description_raw}</div>
         <div className="gallary">
-          {screen.results.map((screen) => (
-            <img src={screen.image} alt="image" key={screen.id} />
-          ))}
+          {screen.results &&
+            screen.results.map((screen) => (
+              <img src={screen.image} alt="" key={screen.id} />
+            ))}
         </div>
-      </div>
-    </div>
+      </Details>
+    </CardShadow>
   );
 }
 
+const CardShadow = styled(motion.div)`
+  width: 100%;
+  min-height: 100vh;
+  overflow-y: scroll;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  &::-webkit-scrollbar{
+      width: 0.5rem;
+    }
+    &::-webkit-scrollbar-thumb{
+      background-color: #3d98bb;
+    }
+`;
+
+const Details = styled(motion.div)`
+  width: 80%;
+  border-radius: 1rem;
+  padding: 2rem 20rem;
+  background: #fff;
+  position: absolute;
+  color: #000;
+  left: 10%;
+  img{
+    width: 100%;
+  }
+`;
 export default GameDetail;
